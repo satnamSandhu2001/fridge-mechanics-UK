8b79f830efb1988e0c0a0225dfb047ada6c075a1
Satnam Sandhu <satnamsandhu70002@gmail.com>
2025-03-24 16:29:13 +0530

    images updated

--- src/components/home/LogosMarquee.tsx
+++ src/components/home/LogosMarquee.tsx
@@ -39,7 +39,7 @@ function LogosMarquee() {
 						key={index}
 						src={logo}
 						alt={`Logo ${index + 1}`}
-						className="h-10 w-auto object-contain"
+						className="h-12 w-auto object-contain"
 					/>
 				))}
 			</motion.div>
