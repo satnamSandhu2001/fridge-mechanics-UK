bf492203227853a253cd22e7eef19e9be383dbdd
Satnam Sandhu <satnamsandhu70002@gmail.com>
2025-04-03 17:19:04 +0530

    smtp updated

--- src/actions/mail.action.ts
+++ src/actions/mail.action.ts
@@ -3,9 +3,8 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-	host: 'smtp.gmail.com',
-	service: 'gmail',
-	port: 465,
+	host: process.env.SMTP_HOST,
+	port: Number(process.env.SMTP_PORT),
 	secure: true,
 	auth: {
 		user: process.env.SMTP_USER,
